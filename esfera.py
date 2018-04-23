94d92cd58b2b72ac8a3d4a64a0b183999f0b0752
RamonReis <ramonreis00@hotmail.com>
2018-04-23 17:32:18 -0300

    Adding code, unfinished

---
+++ esfera.py
@@ -0,0 +1,55 @@
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+import math
+import sys
+from math import *
+from ctypes import *
+Xlib = CDLL("libX11.so.6")
+display = Xlib.XOpenDisplay(None)
+(root_id, child_id) = (c_uint32(), c_uint32())
+#w = Xlib.XRootWindow(display, c_int(0))
+(root_x, root_y, win_x, win_y) = (c_int(), c_int(), c_int(), c_int())
+mask = c_uint()
+
+#mouse cursor positions
+#ret = Xlib.XQueryPointer(display, c_uint32(w), byref(root_id), byref(child_id), byref(root_x), byref(root_y), byref(win_x), byref(win_y), byref(mask))
+#print root_x, root_y#print the values
+
+def cilindro():
+    #Drawing the base    
+    posx, posy = 0,0    
+    sides = 100  
+    radius = 1    
+    
+    for j in range(0, 101):
+        glBegin(GL_POLYGON)    
+        for i in range(100):    
+            cosine = radius * cos(i*2*pi/sides) + posx    
+            sine  = radius * sin(i*2*pi/sides) + posy    
+            glVertex3f(cosine,sine, j/10000)
+        glEnd()
+
+def abacaxi():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    cilindro()
+    glutSwapBuffers()
+
+def timer(i):
+    glutPostRedisplay()
+    glutTimerFunc(50,timer,1)
+
+# PROGRAMA PRINCIPAL
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
+glutInitWindowSize(800,600)
+glutCreateWindow("Esfera")
+glutDisplayFunc(abacaxi)
+glEnable(GL_MULTISAMPLE)
+glEnable(GL_DEPTH_TEST)
+glClearColor(0.,0.,0.,1.0)
+gluPerspective(40,800.0/600.0,0.5,100.0)
+glTranslatef(0,0,-10)
+glRotatef(95,45,0,0)
+glutTimerFunc(50,timer,1)
+glutMainLoop()
